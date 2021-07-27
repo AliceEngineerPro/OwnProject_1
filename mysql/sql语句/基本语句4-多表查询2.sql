8e66e3803744ea2c62240af6b9f61f43f7c800ca
Amfc <longloved@yeah.net>
2021-07-27 13:21:18 +0800

    多表查询2

--- mysql/sql语句/基本语句4-多表查询2.sql
+++ mysql/sql语句/基本语句4-多表查询2.sql
@@ -1,1 +1,31 @@
-create table  tableA ()
+create table  tableA (
+    id int primary key ,
+    name varchar(20)
+
+) ;
+
+create table tableB(
+    id int primary key ,
+    name varchar(20) ,
+    tableA_id int
+) ;
+
+insert into tableA values (1,'Alex'),
+                          (2,'Jack'),
+                          (3,'Amy') ;
+
+insert into tableB values (1,'小雨',1),
+                          (2,'彬彬',2),
+                          (3,'洲洲',4) ;
+
+# tableA
+select * from tableA ;
+# tableB
+select * from tableB ;
+
+# all
+select * from tableA, tableB ;
+
+select * from tableA, tableB where tableB.tableA_id=tableA.id ;
+
+
