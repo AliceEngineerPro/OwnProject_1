845fac3559a3f780f2f1f72d4f5f0b2dfb907ed4
Amfc <longloved@yeah.net>
2021-08-23 13:23:48 +0800

    dev_1

--- python/django_learn/learn/django_node/django/obj/django_temp1_demo1/classes/urls.py
+++ python/django_learn/learn/django_node/django/obj/django_temp1_demo1/classes/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url('select/', views.select_class, name='select_class'),
     url('add/', views.add_class, name='add_class'),
-    url('del', views.del_class, name='del_class')
+    url('del', views.del_class, name='del_class'),
+
+
 
 ]
