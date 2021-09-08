375b99af0eee544a74d27057f9dd221f6699360a
amfc-admin <longlovedtt@gmail.com>
2021-09-08 13:20:16 +0800

    连表查询sql

--- mysql/#local/@localhost.sql
+++ mysql/#local/@localhost.sql
@@ -5,3 +5,9 @@ select concat('drop table ', table_name,';') from information_schema.TABLES wher
 use eg_ajax ;
 show tables ;
 
+select class.name, student.name, teacher.name
+from ajax_student as student
+         left join ajax_classes as class on student.class_id = class.id
+         left join ajax_teachers as teacher
+         left join ajax_classes_to_teachers as ct on ct.teachers_id = teacher.id on ct.classes_id = class.id
+where student.name = '卢平';
