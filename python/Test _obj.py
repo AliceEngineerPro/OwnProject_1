955925e6d909189032e49a6fe7c929aff4449f2b
AliceEngineer <alice.engineer@foxmail.com>
2021-09-25 23:30:23 +0800

    :art:

--- python/Test _obj.py
+++ python/Test _obj.py
@@ -35,28 +35,4 @@ def sum_input():
 print(sum_input())
 
 
-requests.request(
-    # 常用
-    method='',  # 提交方式
-    url='',  # 提交地址
-    params='',  # 在GET中的请求参数  url?k1=v1&k2=bv2
-    data='',  # 在请求体中的传递数据  {'user': 'user', 'passwd': '123'}
-    json='',  # 在请求体中的传递数据  {"user": "user1", "passwd": "123"}
-    headers='',  # 请求头
-    cookies='',  # cookies
-    # 特殊
-    files='',  # 上传文件
-    auth='',  # 封装请求头, 多用于登录验证加密
-    timeout='',  # 超时(connect time, read time)
-    allow_redirects=True,  # 是否允许重定向
-    proxies='',  # 代理
-    verify=True,  # 是否忽略ssl证书
-    stream='',  # 保存在磁盘
-    cert='',  # ssl证书
 
-)
-
-headers = {
-    'Referer': url,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
-}
--- python/python_爬虫/day/day2/token/获取token.py
+++ python/python_爬虫/day/day2/token/获取token.py
@@ -1,1 +1,41 @@
-import requestsimport jsonurl_list = [    # 'https://github.com',    'https://baidu.com',]proxy = {    'http': 'http://127.0.0.1:1080',}# def url_index(url):#     address = ''#     for index in url:#         address = index#     return addressresponse = requests.request(    method='GET',    # url=url_index(url_list)    url=url_list[0],    # proxies=proxy,    allow_redirects=True,    timeout=(500, 3000),    verify=True,)# print(response.text)print(response.headers, '\n', type(str(response.headers)))headers = json.loads(str(response.headers))print(headers)
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+url_list = [
+    # 'https://github.com',
+    'https://baidu.com',
+]
+
+proxy = {
+    'http': 'http://127.0.0.1:1080',
+}
+
+# def url_index(url):
+#     address = ''
+#     for index in url:
+#         address = index
+#     return address
+
+
+response = requests.request(
+    method='GET',
+    # url=url_index(url_list)
+    url=url_list[0],
+    # proxies=proxy,
+    allow_redirects=True,
+    timeout=(500, 3000),
+    verify=True,
+)
+
+response.encoding = response.apparent_encoding
+soup = BeautifulSoup(response.text, features='lxml')
+tag_a = soup.find_all(name='a')
+for a in tag_a:
+    tag_a_attrs = a.attrs.get('href')
+    print(tag_a_attrs)
+
+# response.headers = dict(response.headers)
+# headers = json.dumps(response.headers, indent=4)
+# print(headers)
